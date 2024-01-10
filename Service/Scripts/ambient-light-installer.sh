ed70ffb5c0c132c23e0d6f8fb2f4d830b6246c8c
gpinchon <pinchon.guillaume@gmail.com>
2024-01-10 22:19:02 +0100

    enable service on install

--- Service/Scripts/ambient-light-installer.sh
+++ Service/Scripts/ambient-light-installer.sh
@@ -7,7 +7,10 @@ echo "Copying ambient-light.service to /etc/systemd/system/ambient-light.service
 sudo sh -c "cp '$SOURCE'/ambient-light.service /etc/systemd/system/ambient-light.service"
 echo "Copying ambient-light to /usr/bin/ambient-light"
 sudo sh -c "cp '$SOURCE'/ambient-light /usr/bin/ambient-light"
-echo "Starting Ambient Light service"
+echo "Relaunching systemd"
 systemctl daemon-reload
+echo "Starting Ambient Light service"
 systemctl start ambient-light.service
+echo "Enabling Ambient Light service"
+systemctl enable ambient-light.service
 echo "Installation done !"
